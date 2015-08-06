d5aa38519c46734c9a68b42cf166a222a8461384
edinchia777 <edinson.taju@gmail.com>
2015-08-06 16:58:30 -0500

    AlfaMudanzas

---
+++ logs/error.php
@@ -0,0 +1,9 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2015-05-16 14:48:19 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2015-05-16T14:48:19+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2015-05-24T19:48:00+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2015-06-18T17:24:05+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
